2bc509fce87f152865e838678ce57a76ffba7d6a
KimPaulson <78212843+KimPaulson@users.noreply.github.com>
2024-09-01 00:05:37 -0500

    Add files via upload

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,6 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Congrats, you won! Wanna play again?";
+    public static final String ERROR = "Opps, looks like an error has occured..";
+}
